6a66aeadcbe7021e323fd27274e107ce113fe7b9
Sabin Subedi <sabinsubedi125@gmail.com>
2023-01-15 20:39:22 +0545

    added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element:
+//   }
+// ])
 
 // This is the container app, which is the main app that will be rendered in the browser
 function App() {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />;
+    </BrowserRouter>
+  );
 }
 
 export default App;
