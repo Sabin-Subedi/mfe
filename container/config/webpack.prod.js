ee3e8bf221fbbe017979c53416200596d5ded21d
Sabin Subedi <sabinsubedi125@gmail.com>
2023-01-15 19:02:44 +0545

    update to the workflow

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@https://${domain}/marketing/latest/remoteEntry.js}`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js}`,
       },
       shared: packageJson.dependencies,
     }),
