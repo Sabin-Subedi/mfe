d23dbe2b4bd47878b013ca06e000fc38f8b6da9f
Sabin Subedi <sabinsubedi125@gmail.com>
2023-01-15 16:10:18 +0545

    minor update

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
+// This is the container app, which is the main app that will be rendered in the browser
 function App() {
   return <MarketingApp />;
 }
